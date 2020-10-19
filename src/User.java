9e9d5d5e3c9e4cebf2d380b787eabf7d0e8754a5
Patterson Howell <pattersonhowell@Pattersons-MacBook-Pro.local>
2020-10-19 09:56:39 -0400

    updated user stub

--- src/User.java
+++ src/User.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class User {
 
+	private String name;
+	private String password;
+	private String address;
+	private String phone;
+	private String email;
+	public ArrayList<Review> reviews;
+	
+	public String toString() {
+		return "";
+	}
+	
 }
