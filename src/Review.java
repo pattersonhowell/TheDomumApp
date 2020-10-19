39c1f0ef94dc3854744f4d0127f18e230e2e3ca8
khegs99 <kmhagan@email.sc.edu>
2020-10-19 16:45:36 -0400

    Added review Stub

---
+++ src/Review.java
@@ -0,0 +1,23 @@
+
+public class Review {
+	public String author; 
+	public String date;
+	public String reviewText;
+	public int rating; 
+	
+	public Review(int rating, String author, String date,String reviewText) {
+		this.rating = rating; 
+		this.author = author;
+		this.date = date;
+		this.reviewText = reviewText; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
