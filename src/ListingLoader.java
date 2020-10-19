7fe5a215ea86eb051c1e734531ddad4e8449868c
Patterson Howell <pattersonhowell@Pattersons-MacBook-Pro.local>
2020-10-19 10:30:51 -0400

    listing loader and writer stub

---
+++ src/ListingLoader.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class ListingLoader {
+
+	public ArrayList<Listing> loadListings() {
+		//return listings
+	}
+}
---
+++ src/ListingWriter.java
@@ -0,0 +1,11 @@
+
+public class ListingWriter {
+
+	public void writeListings() {
+		
+	}
+	
+	public JSONObject getListingJSON(Listing listing) {
+		//return JSON
+	}
+}
