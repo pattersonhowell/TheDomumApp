6edc6b49cdc8ba390b6edf968ee10ca8c3b8a230
Patterson Howell <pattersonhowell@Pattersons-MacBook-Pro.local>
2020-10-19 10:08:51 -0400

    student stub

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Student {
+
+	private String studentID;
+	private ArrayList<Listing> favoriteListings;
+	
+	public Student(String studentID) {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
+	
+	public void leaveReview(Review review) {
+		//leave a review
+	}
+	
+	public void favoriteListing(Listing listing) {
+		//favorite a listing
+	}
+	
+}
