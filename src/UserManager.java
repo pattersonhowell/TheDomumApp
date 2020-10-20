2f6f19b38832ec6d7cd28fa23aad64b82a6a5b5d
ThomasH428 <70242630+ThomasH428@users.noreply.github.com>
2020-10-20 10:55:14 -0400

    Update UserManager.java

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
 public class UserManager {
 
+	private ArrayList<User> users;
+	
+	public void addUser(User user) {
+		
+	}
+	
+	public void removeUser(User user) {
+		
+	}
+	
+	public void checkUser(User user) {
+		
+	}
 }
