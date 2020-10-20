4e1180a8b1d4827560118c0a85ac11baf9cb1dee
ThomasH428 <70242630+ThomasH428@users.noreply.github.com>
2020-10-20 10:54:52 -0400

    Update System.java
    updating System stub

--- src/System.java
+++ src/System.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
 
 public class System {
 
+	private ArrayList<String> users;
+	private ListingManager listingManager;
+	private UserManager userManager;
+	
+	public boolean verifiedLogin(String name, String password) {
+		
+	}
+	
+	public void signUp(String name, String password, String address, String phone, String email) {
+		
+	}
+	
+	public ArrayList<Listing> browseAnonymously(){
+		
+	}
+	
+	public void deleteAccount(String name, String password) {
+		
+	}
 }
