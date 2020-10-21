0b5f06220d93c2bf144cbffed5234bb9725d737d
khegs99 <kmhagan@email.sc.edu>
2020-10-21 18:43:04 -0400

    Made search methods for every attribute

--- src/ListingManager.java
+++ src/ListingManager.java
@@ -12,40 +12,72 @@ public class ListingManager {
 	public ArrayList<Listing> showAllListings() {
 		return listings; 
 	}
-	public Listing findListing(int listingId) {
-		
-		
+	public Listing findListing(int listingId) {			
 		for (Listing listing: listings) {
 			if(listing.getListingID()==listingId) {
 				return listing;
 			}
 		}
 		System.out.println("Could not find Listing with that ID");
-		return null;
-		
+		return null;		
 	}
 	
-	public ArrayList<Listing> priceSearch(double price) {
-		return null;//TODO Implement search method 
+	public ArrayList<Listing> priceSearch(double price) {	
+		ArrayList<Listing> mathchingListings = new ArrayList<Listing>();
+		for (Listing listing: listings) {
+			if(listing.getPrice() <= price) {
+				mathchingListings.add(listing);
+			}
+		}		
+		return mathchingListings; 
 	}
 	
 	public ArrayList<Listing> distFromCampusSearch(double distance) {
-		return null;//TODO Implement search method 
+		ArrayList<Listing> mathchingListings = new ArrayList<Listing>();
+		for (Listing listing: listings) {
+			if(listing.getDistFromCampus() <= distance) {
+				mathchingListings.add(listing);
+			}
+		}		
+		return mathchingListings; 
 	}
 	
 	public ArrayList<Listing> numBathSearch(int numBath) {
-		return null;//TODO Implement search method 
+		ArrayList<Listing> mathchingListings = new ArrayList<Listing>();
+		for (Listing listing: listings) {
+			if(listing.getNumBathroom() <= numBath) {
+				mathchingListings.add(listing);
+			}
+		}		
+		return mathchingListings; 
 	}
 	public ArrayList<Listing> numBedSearch(int numBed) {
-		return null;//TODO Implement search method 
+		ArrayList<Listing> mathchingListings = new ArrayList<Listing>();
+		for (Listing listing: listings) {
+			if(listing.getNumBedroom() <= numBed) {
+				mathchingListings.add(listing);
+			}
+		}		
+		return mathchingListings; 
 	}
 	public ArrayList<Listing> yearBuiltSearch(int yearBuilt) {
-		return null;//TODO Implement search method 
+		ArrayList<Listing> mathchingListings = new ArrayList<Listing>();
+		for (Listing listing: listings) {
+			if(listing.getYearBuilt() <= yearBuilt) {
+				mathchingListings.add(listing);
+			}
+		}		
+		return mathchingListings; 
 	}
 	public void printListings() {
-		//print all listings 
+		System.out.println("Printing Listings");
+		for (Listing listing: listings) {
+			System.out.println(listing.toString());
+		}		
 	}
 	public void editListing(Listing listing) {
+		
+		
 		
 	}
 	
