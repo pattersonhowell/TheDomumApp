b22c051e96e585468c4216825d033314aa8bee51
ThomasH428 <70242630+ThomasH428@users.noreply.github.com>
2020-10-21 21:12:42 -0400

    Update UserManager.java

--- src/UserManager.java
+++ src/UserManager.java
@@ -2,17 +2,22 @@ import java.util.ArrayList;
 
 public class UserManager {
 
-	private ArrayList<User> users;
+	public ArrayList<User> users;
+	
+	public UserManager() {
+		ArrayList<User> users = new ArrayList<User>();
+	}
 	
 	public void addUser(User user) {
-		
+		users.add(user);
 	}
 	
 	public void removeUser(User user) {
-		
+		users.remove(user);
 	}
 	
-	public void checkUser(User user) {
-		
+	public boolean userTaken(User user) {
+		boolean found = users.contains(user);
+		return found;
 	}
 }
