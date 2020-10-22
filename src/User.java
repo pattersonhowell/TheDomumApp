2c2ebee0405a9f2def3b12970baf4971edaa109e
Patterson Howell <pattersonhowell@Pattersons-MacBook-Pro.local>
2020-10-21 20:29:18 -0400

    app and agreement

--- src/User.java
+++ src/User.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class User {
-
+	
+	
+	
 	private String id;
 	private String name;
 	private String password;
@@ -13,6 +18,31 @@ public class User {
 	public String toString() {
 		return "ID: " + id + "\nName: " + name + "\nPassword: " + password + "\nAddress: " 
 				+ address + "\nPhone: " + phone + "\nEmail: " + email;
+	}
+	
+	public void generateUserAgreement() throws FileNotFoundException {
+		
+		Scanner fileIn = new Scanner(new File("LeasingAgreement.txt"));
+		
+		while(fileIn.hasNextLine()) {
+			
+			String userAgreement = fileIn.nextLine();
+			System.out.println(userAgreement);
+			
+		}
+		fileIn.close();
+	}
+	
+	public void generateApplication() throws FileNotFoundException {
+		
+		Scanner fileIn = new Scanner(new File("Application.txt"));
+		
+		while(fileIn.hasNextLine()) {
+			
+			String application = fileIn.nextLine();
+			System.out.println(application);
+		}
+		fileIn.close();
 	}
 	
 }
