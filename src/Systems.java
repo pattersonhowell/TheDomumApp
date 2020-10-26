6ef8d3ff565873658a800a33113b4d1d4cf9db87
Patterson Howell <pattersonhowell@Pattersons-MacBook-Pro.local>
2020-10-26 17:10:40 -0400

    made singletons, fixed SignUp and verified login

--- src/Systems.java
+++ src/Systems.java
@@ -3,34 +3,46 @@ import java.util.ArrayList;
 
 public class Systems {
 
-	private ArrayList<User> users;
+	//private ArrayList<User> users;
 	private ListingManager listingManager;
-	private UserManager userManager;
+	//private UserManager userManager;
+	UserManager manager = UserManager.getInstance();
 	
 	public boolean verifiedLogin(String name, String password) {
 		boolean exists = false;
 		int index;
 		String reqPassword;
 		
-		if (users.contains(name)) {
-			index = users.indexOf(name);
+		for(int i = 0; i < manager.users.size(); i++) {
+			if (manager.users.get(i).getName().contains(name)) {
+				System.out.println("found it");
+				//index = i;
 
-			reqPassword = users.get(index).getPassword();
+				reqPassword = manager.users.get(i).getPassword();
+				
+				if (password.equals(reqPassword))
+					exists = true;
 
-			
-			if (password.equals(reqPassword))
-				exists = true;
+			}
 		}
+	
 		return exists;
 	}
 	
 	public void signUp(String name, String password, String address, String phone, String email) {
-		int id = users.size()+1;
+		
+		UserManager manager = UserManager.getInstance();
+		
+		int id = 0; //users.size()+1;
 
 		ArrayList<Review> reviews = new ArrayList<Review>();
 
 		
-		User newUser = new User(id, name, password, address, phone, email, reviews);
+		//User newUser = new User(id, name, password, address, phone, email);//reviews);
+		manager.addUser(id, name, password, address, phone, email);
+		System.out.println(manager.users.size());
+		System.out.println(manager.users.get(0).getName());
+		
 	}
 	
 	public ArrayList<Listing> browseAnonymously(){
@@ -39,7 +51,7 @@ public class Systems {
 	}
 	
 	public void deleteAccount(String name, String password) {
-		int index = users.indexOf(name);
-		users.remove(index);
+		int index = manager.users.indexOf(name);
+		manager.users.remove(index);
 	}
 }
