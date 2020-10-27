11bf13ca37683c76a110afd60e2052f655b58edb
khegs99 <kmhagan@email.sc.edu>
2020-10-27 09:37:53 -0400

    Changed Edit listing to multiple methods

--- src/ListingManager.java
+++ src/ListingManager.java
@@ -1,9 +1,6 @@
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ListingManager {
-	Scanner kb = new Scanner(System.in);
 	private ArrayList<Listing> listings = ListingLoader.loadListings();
 	
 	public void addListing(Listing listing) {
@@ -69,9 +66,7 @@ public class ListingManager {
 			if(listing.getYearBuilt() <= yearBuilt) {
 				mathchingListings.add(listing);
 			}
-		}		
-		
-		
+		}				
 		return mathchingListings; 
 	}
 	public void printListings() {
@@ -80,66 +75,64 @@ public class ListingManager {
 			System.out.println(listing.toString());
 		}		
 	}
-	public void editListing(Listing listing) {
-		System.out.println("Displaying the current Listing:");
-		listing.toString();
-		System.out.println("What would you like to edit");
-		System.out.println(""
-				+ "Press 1 to edit agent ID "
-				+ "\nPress 2 to edit price"
-				+ "\nPress 3 to edit address"
-				+ "\nPress 4 to edit the number of bedrooms "
-				+ "\nPress 5 to edit the number of bathrooms"
-				+ "\nPress 6 to edit the year built "
-				+ "\nPress 7 to edit the distance from campus"
-				+ "\nPress 8 to edit the number avalible");
-		int choice = kb.nextInt();		
-		switch(choice) {
-		case 1:		
-			System.out.println("Please enter the modified agent ID");
-			String agent = kb.nextLine();
-			listing.setAgentID(agent);
-			break; 			
-		case 2: 	
-			System.out.println("Please enter the modified price");
-			double price = kb.nextDouble();
-			listing.setPrice(price);
-			break;		
-			
-		case 3:		
-			System.out.println("Please enter the modified address");
-			String address = kb.nextLine();
-			listing.setAddress(address);
-			break;
-						
-		case 4:		
-			System.out.println("Please enter the modified number of bedrooms");
-			int numBed = kb.nextInt();
-			listing.setNumBedroom(numBed);
-			break;
-		
-		case 5:
-			System.out.println("Please enter the modified number of bathrooms");
-			int numbath = kb.nextInt();
-			listing.setNumBathroom(numbath);
-			break;
-		case 6:
-			System.out.println("Please enter the modified number of year built");
-			int yearBuilt = kb.nextInt();
-			listing.setYearBuilt(yearBuilt);
-			break;
-		case 7: 
-			System.out.println("Please enter the modified distance from campus");
-			double distance = kb.nextDouble();
-			listing.setDistFromCampus(distance);
-			break;
-		case 8:
-			System.out.println("Please enter the modified number avalible");
-			int numAvalible = kb.nextInt();
-			listing.setNumberAvalible(numAvalible);
-			break;
-	
+	public void editAgentID(Listing listing, String ID) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setAgentID(ID);
+			}
 		}			
 	}	
-}
+	public void editPrice(Listing listing, double price) {		
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setPrice(price);
+			}
+		}
+	}	
+	public void editAddress(Listing listing, String address) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setAddress(address);
+			}
+		}	
+	}
+	public void editNumberBed(Listing listing, int numBed) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setNumBedroom(numBed);
+			}
+		}		
+	}
+	public void editNumberBath(Listing listing, int numBath) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setNumBathroom(numBath);
+			}
+		}	
+		
+		
+		
+	}	
+	public void editYearBuilt(Listing listing, int yearBuilt) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setYearBuilt(yearBuilt);
+			}	
+		}
+	}	
+	public void editDistanceFromCampus(Listing listing, double distance) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setDistFromCampus(distance);
+			}
+		}
+	}		
+	public void enterNumAvalible(Listing listing, int numAvalible) {
+		for(Listing x:listings) {
+			if(x==listing) {
+				x.setNumberAvalible(numAvalible);
+			}
+		}			
+	}
+}			
 
