ee6a6370ce9cf05bc3271f01ec5a6a5334c8c74c
khegs99 <kmhagan@email.sc.edu>
2020-11-13 12:39:25 -0500

    4 tests complete

--- src/ListingManagerTest.java
+++ src/ListingManagerTest.java
@@ -8,84 +8,64 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ListingManagerTest {
-	
-	  private ListingManager listingManager = ListingManager.getInstance();
-	  private ArrayList<Listing> listingList = listingManager.getListings();
-	
-	@BeforeEach
+/**
+ * Class that tests functionality of ListingManager
+ * @author Kevin Hagan *
+ */
+public class ListingManagerTest {	
+	  private ListingManager listingManager = ListingManager.getInstance();	  
+	@Before 
 	public void setup() throws Exception {		
-			
-		
-		
-		
+		listingManager.getListings().clear();
+		ArrayList<Review> nullRev = new ArrayList<Review>();//so we can add object and avoid null pointer
+		ArrayList<Suite> nullSuite = new ArrayList<Suite>();//so we can add object and avoid null pointer
+		listingManager.addListing(new Listing("Blue House",01,"00406","672 School Drive",nullRev,nullSuite,1960,4,true,false,true,false,true,true));
+		listingManager.addListing(new Listing("Green House",02,"00501","555 Pine Rd ",nullRev,nullSuite,2011,2,true,false,true,false,true,true));
+		ListingWriter.writeListings();		
 	}
 
-	@AfterEach
+	@After
 	public void tearDownAfterClass() throws Exception {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-
+		listingManager.getListings().clear();
+		ListingWriter.writeListings();//per video on Junit Library system 					
+	}	
 	
-
+	@Test		
+	public void testSize() {		
+		assertEquals(2,listingManager.getListings().size());		
+	}	
+	
 	@Test
-	public void testAddListing() {
-		ArrayList<Review> blueHouseRev = new ArrayList<Review>();		
-		ArrayList<Suite> blueHouseSuite = new ArrayList<Suite>();
-		Listing blueHouse = new Listing("Blue House",5555,"72892","672 School Drive",blueHouseRev,blueHouseSuite,1960,4,true,false,true,false,true,true);
+	public void testAddListing() {				
+		ArrayList<Review> redHouseRev = new ArrayList<Review>();		
+		ArrayList<Suite> redHouseSuite = new ArrayList<Suite>();
+		Listing redHouse = new Listing("Red House",123,"1232","3 Acorn Lane",redHouseRev,redHouseSuite,2003,5,true,false,true,false,true,true);
 		int previousSize =listingManager.getListings().size();
-		listingManager.addListing(blueHouse);	
+		listingManager.addListing(redHouse);	
 		assertEquals(previousSize+1,listingManager.getListings().size()); 
-		listingManager.removeListing(5555);//remove the house that we added in the test 
-
 	}
+	
 	@Test
-	public void testRemoveListing() {	
-					
-		//addding a listing to be removed 
-				ArrayList<Review> greenHouseRev = new ArrayList<Review>();		
-				ArrayList<Suite> GreenHouseSuite = new ArrayList<Suite>();
-				Listing greenHouse = new Listing("Green House",233,"72892","672 School Drive",greenHouseRev,GreenHouseSuite,1960,4,true,false,true,false,true,true);
-				listingManager.addListing(greenHouse);	
-		
-		
-		
-//		
-//			int previousSize = listingManager.getListings().size();
-//			listingManager.removeListing(233);
-//			assertEquals(previousSize-1,listingManager.getListings().size());
-	}
-	
-	public void testListingExists() {
-			ArrayList<Listing> listings = listingManager.getListings();
-		
-			int listingID = 5555;
-		
-			//this method will search for a listing with a listing id and see if it returns the same things as the method 
+	public void testRemoveListing() {						
 			
-		
-		
-		
-		
+	int previousSize = listingManager.getListings().size();	
+	listingManager.removeListing(01);//removing blue house which is added in the setUp
+	assertEquals(previousSize-1,listingManager.getListings().size());
 	}
 	
-	public void testComprehensiveSearch() {
-		
-		//this will pass in a numch of variables and see if comprehensive search works idk 
-		
-		
-		
+	@Test
+	public void testListingExists() {	
+	assertEquals(true,listingManager.listingExists(02));//listing id for green house 			
 	}
-	
+//	
+//	public void testComprehensiveSearch() {
+//		
+//		//this will pass in a numch of variables and see if comprehensive search works idk 
+//		
+//		
+//		
+//	}
+//	
 	
 	
 	
