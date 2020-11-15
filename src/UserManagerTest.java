8fa91117cd7fe394ec52f7a8a0baf5367b1be44c
Patterson Howell <pattersonhowell@gmail.com>
2020-11-15 08:58:02 -0500

    fixed before/after (missing imports)

--- src/UserManagerTest.java
+++ src/UserManagerTest.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 /**
  * testing class for user manager
  * @author Thomas
