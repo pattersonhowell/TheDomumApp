8fe6b52b2ff8e0133627bdd848e1311eefc9bab6
ThomasH428 <70242630+ThomasH428@users.noreply.github.com>
2020-11-14 21:18:06 -0500

    Update UserManagerTest.java
    mostly finished UserManagerTest

--- src/UserManagerTest.java
+++ src/UserManagerTest.java
@@ -1,28 +1,28 @@
 import static org.junit.Assert.*;
-
-import org.junit.BeforeClass;
+import java.util.ArrayList;
 
 /**
  * testing class for user manager
- * 
  * @author Thomas
  */
 public class UserManagerTest {
 	private UserManager userManager = UserManager.getInstance();
 	
 	/**
-	 * 
+	 * adds users ArrayList and some test users after clearing any existing data
 	 */
 	@Before
 	public void setup() throws Exception {
 		userManager.getUsers().clear();
-		userManager.addUser(new User("01", "Dom", "domum", "123 domum st", "12345678910", "domumtest@gmail"));
+		ArrayList<User> users = new ArrayList<User>();
+		User dom = new User("01", "Dom", "domum", "123 domum st", "12345678910", "domumtest@gmail");
+		userManager.addUser(dom);
 		userManager.addUser(new User("02", "Mum", "mumod", "123 mumod st", "01987654321", "mumodtest@gmail"));
 		UserWriter.writeUser();
 	}
 	
 	/**
-	 * 
+	 * clears users array list
 	 */
 	@After
 	public void tearDown() {
@@ -30,11 +30,46 @@ public class UserManagerTest {
 		UserWriter.writeUser();
 	}
 	
+	/**
+	 * tests if a user is being added by getting the users ArrayList size before and after a user is added
+	 */
 	@Test
 	public void testAddUser() {
 		User jackie = new User("03", "Jackie", "jackie123", "52 Sesame Street", "5923908476", "jackie@aol");
 		int originalSize = userManager.getUsers().size();
 		userManager.addUser(jackie);
 		assertEquals(originalSize+1, userManager.getUsers().size());
+	}
+	
+	/**
+	 * tests if a user is being removed by getting the users ArrayList size before and after a user is removed
+	 */
+	@Test
+	public void testRemoveUser() {
+		int originalSize = userManager.getUsers().size();
+		User dom = new User("01", "Dom", "domum", "123 domum st", "12345678910", "domumtest@gmail");
+		userManager.removeUser(dom);
+		assertEquals(originalSize-1, userManager.getUsers().size());
+	}
+	
+	/**
+	 * tests if a taken user name returns true
+	 */
+	@Test
+	public void testUserTakenTrue() {
+		User thomas = new User("04", "Thomas", "thomas123", "61 Bride Way", "8694038175", "toams@hotmail");
+		ArrayList<User> users = new ArrayList<User>();
+		boolean originalTaken = users.contains(thomas);
+		users.add(thomas);
+		assertTrue(userManager.userTaken(thomas));
+	}
+	
+	/**
+	 * tests if a non existing user returns false
+	 */
+	@Test
+	public void testUserTakenFalse() {
+		User zach = new User("05", "Zach", "zach123", "0152 Ball St", "9658721096", "zach@hotmail");
+		assertFalse(userManager.userTaken(zach));
 	}
 }
